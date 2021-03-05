3696162d390eab8318cf7bdcd52c87cf53e5b7d6
jmckain <JamesMcKain@quickenloans.com>
2021-03-05 11:48:25 -0500

    installed simple-js-lib

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import {sayHello, sayGoodbye} from '@jjmckain/simple-js-lib';
 
 function App() {
+	sayHello();
+	sayGoodbye();
   return (
     <div className="App">
       <header className="App-header">
